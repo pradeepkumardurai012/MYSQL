a2cc95a3a6d835b596cdc1b81c22dce6364cc78e
pradeepkumardurai012 <37336521+pradeepkumardurai012@users.noreply.github.com>
2018-04-30 05:02:39 +0000

    Update and rename .gitignore to creating_table.sql

---
+++ .gitignore/creating_table.sql
@@ -0,0 +1,5 @@
+create database students;
+show databases;
+use students;
+create table details (students_id varchar(25),name varchar(50),roll_no varchar(50),department varchar(50),class varchar(50),semester varchar(50),sgpa varchar(50),cgpa varchar(50),projects varchar(50),activities varchar(50));
+describe details;
